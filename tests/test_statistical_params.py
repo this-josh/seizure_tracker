c5610b5bee7ff137a1b608221f26194b8a3a3d61
Josh <joshuaarkirk@hotmail.co.uk>
2020-08-02 17:10:17 +0100

    Changed to use the constructor file

--- tests/test_statistical_params.py
+++ tests/test_statistical_params.py
@@ -3,7 +3,9 @@ import numpy as np
 import datetime as dt
 import pytz
 
-from trackerApp.statistical_params import _remove_outliers, most_recent_seizure, get_clusters
+from constructors import make_df, make_full_df
+
+from trackerApp.statistical_params import _remove_outliers, most_recent_seizure, get_clusters,get_cluster_info
 
 def test_remove_outliers():
     """
@@ -12,13 +14,6 @@ def test_remove_outliers():
     values = pd.Series([-200.4, 0, 10.2, 20, 30, 40, 50, 60, 70, 80, 90, 300])
     values_no_outliers = pd.Series([0.0, 10.2, 20.0, 30.0, 40.0, 50.0, 60.0, 70.0, 80.0, 90.0]).to_numpy()
     np.testing.assert_array_equal(values_no_outliers, _remove_outliers(values).to_numpy())
-
-def make_df(days_ago, df_len)->pd.DataFrame:
-    start_date = dt.datetime.now(pytz.utc)- dt.timedelta(days=days_ago)
-    times = [start_date+dt.timedelta(days=day+1) for day in range(df_len)]
-    values = [[val, val + 1] for val in range(df_len)]
-    df = pd.DataFrame(values, times)
-    return df
 
 
 def test_most_recent_seizure():
@@ -35,16 +30,10 @@ def test_most_recent_seizure():
 
 
 def test_get_clusters():
-    days_ago_start = 100
-    len_cluster = 2
-    df_all = pd.DataFrame()
     num_clusters = 6
-    for val in range(num_clusters):
-        df = make_df(days_ago_start, len_cluster)
-        df_all = df_all.append(df)
-        days_ago_start -= 14
-
-    clusters = get_clusters(df_all)
+    len_cluster = 2
+    df = make_full_df(len_cluster=len_cluster, num_clusters=num_clusters)
+    clusters = get_clusters(df)
     # check it found the six clusters
     assert len(clusters) == num_clusters
 
